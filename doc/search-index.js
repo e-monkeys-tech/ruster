137e6542c0e1efab219b6e8fe23a4e8376da77b2
Pierre Baconnier <pierre_baconnier@carrefour.com>
2021-11-24 21:11:48 +0100

    docs: update docs

--- doc/search-index.js
+++ doc/search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"ruster":{"doc":"","t":[5,5,5,5,5,5,5,5,5],"n":["mysql_restore_all_databases","mysql_restore_database","mysqldump_all_databases","mysqldump_database","pg_dump_all_databases","pg_dump_database","psql_restore_all_databases","psql_restore_database","run"],"q":["ruster","","","","","","","",""],"d":["","","","","","","","",""],"i":[0,0,0,0,0,0,0,0,0],"f":[[[]],[[]],[[]],[[]],[[]],[[]],[[]],[[]],[[]]],"p":[]}\
+"ruster":{"doc":"","t":[5,5,5,5,5,5,5,5],"n":["mysql_restore_all_databases","mysql_restore_database","mysqldump_all_databases","mysqldump_database","pg_dump_all_databases","pg_dump_database","psql_restore_all_databases","psql_restore_database"],"q":["ruster","","","","","","",""],"d":["Example","Example","Example","Example","Example","Example","Example","Example"],"i":[0,0,0,0,0,0,0,0],"f":[[[]],[[]],[[]],[[]],[[]],[[]],[[]],[[]]],"p":[]}\
 }');
 if (window.initSearch) {window.initSearch(searchIndex)};
